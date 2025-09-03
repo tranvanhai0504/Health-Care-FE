2b3884ab12e57c4832c442240c8f5ceb8a4af8c5
tranvanhai0504 <tvh050423@gmail.com>
2025-09-03 13:58:20 +0700

    fix: be link

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
-// export const host = "https://devq-be0x7.site";
-export const host = "http://localhost:8081";
+export const host = "https://devq-be0x7.site";
+// export const host = "http://localhost:8081";
 // export const host = "http://192.168.1.8:8081";
 
 // Export all formatting utilities
